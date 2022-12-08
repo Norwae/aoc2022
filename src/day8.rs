473c12e3747514c3b539e46a5f4a2d4942d38127
Elisabeth Schulz <schulz.elisabeth.terra@gmail.com>
2022-12-08 13:42:17 +0100

    Parallelism

--- src/day8.rs
+++ src/day8.rs
@@ -1,26 +1,23 @@
 use std::cmp::max;
 use nom::IResult;
+use crate::util;
 
 use crate::util::{default_solution, linear2d::Direction, linear2d::Linear2DArray};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Tree {
-    blocked_direction_count: i32,
-    scenic_score: i32,
-    height: i32,
+    blocked: bool,
+    sight_range: u32,
+    height: u32,
 }
 
 impl Tree {
-    fn new(height: i32) -> Self {
+    fn new(height: u32) -> Self {
         Self {
             height,
-            blocked_direction_count: 0,
-            scenic_score: 1,
+            blocked: false,
+            sight_range: 0,
         }
-    }
-
-    fn is_visible(&self) -> bool {
-        self.blocked_direction_count < 4
     }
 }
 
@@ -29,56 +26,84 @@ fn parse(input: &str) -> IResult<&str, Linear2DArray<Tree>> {
     let bytes = input.as_bytes();
     let width = bytes.iter().filter(|b| **b < b'0').count();
     let storage = bytes.into_iter().filter(|b| **b >= b'0' && **b <= b'9').map(|b| {
-        let height = (*b - b'0') as i32;
+        let height = (*b - b'0') as u32;
         Tree::new(height)
     }).collect();
 
     IResult::Ok(("", Linear2DArray::new(storage, width)))
 }
 
-fn compute_solution(mut input: Linear2DArray<Tree>) -> (usize, i32) {
-    #[derive(Debug, Default)]
-    struct SweepState {
-        highest: i32,
-        visible_at_height: [i32; 10],
-        visible: usize,
-        best_scenic_score: i32,
-    }
-    let mut state = SweepState {
-        highest: 0,
-        visible_at_height: [0; 10],
-        visible: input.height * input.width,
-        best_scenic_score: 0,
-    };
-    for dir in Direction::ALL {
-        state = input.sweep(state, dir, |state| {
-            state.highest = -1;
-            state.visible_at_height.fill(0);
-            true
-        }, |state, _idx, tree| {
-            let height = tree.height;
-            if state.highest >= height {
-                tree.blocked_direction_count += 1;
+#[derive(Debug, Default)]
+struct SweepState {
+    highest: i32,
+    visible_at_height: [u32; 10],
+}
 
-                if !tree.is_visible() {
-                    state.visible -= 1
+async fn compute_sweep(mut trees: Linear2DArray<Tree>, direction: Direction) -> Linear2DArray<Tree> {
+    let mut state = SweepState::default();
+    trees.sweep(&mut state, direction, |state| {
+        state.highest = -1;
+        state.visible_at_height.fill(0);
+        true
+    }, |state, _idx, tree| {
+        let height = tree.height as i32;
+        if state.highest >= height {
+            tree.blocked = true;
+        } else {
+            state.highest = height;
+        }
+
+        let height = height as usize;
+        tree.sight_range = state.visible_at_height[height];
+
+        (&mut state.visible_at_height[0..=height]).fill(1);
+        (&mut state.visible_at_height[(1 + height)..]).iter_mut().for_each(|value| *value += 1);
+
+        true
+    });
+
+    trees
+}
+
+fn compute_solution(input: Linear2DArray<Tree>) -> (u32, u32) {
+    let west = util::parallel::in_parallel(
+        compute_sweep(input.clone(), Direction::WestToEast)
+    );
+    let north = util::parallel::in_parallel(
+        compute_sweep(input.clone(), Direction::NorthToSouth)
+    );
+    let east = util::parallel::in_parallel(
+        compute_sweep(input.clone(), Direction::EastToWest)
+    );
+
+    let south = util::parallel::in_parallel(
+        compute_sweep(input, Direction::SouthToNorth)
+    );
+
+    let finish = async move {
+        if let (Ok(west), Ok(north), Ok(east), Ok(south)) = tokio::join!(west, north, east, south) {
+            let mut total_visible = 0u32;
+            let mut best_scenery_score = 0u32;
+            for (((a, b), c), d) in west.iter().zip(north.iter()).zip(east.iter()).zip(south.iter()) {
+                let visible = !(a.blocked && b.blocked && c.blocked && d.blocked);
+                let scenery_score = a.sight_range * b.sight_range * c.sight_range * d.sight_range;
+
+                if visible {
+                    total_visible += 1
                 }
-            } else {
-                state.highest = height;
+                if scenery_score > best_scenery_score {
+                    best_scenery_score = scenery_score
+                }
             }
 
-            let height = height as usize;
-            tree.scenic_score *= state.visible_at_height[height];
-            state.best_scenic_score = max(tree.scenic_score, state.best_scenic_score);
+            (total_visible, best_scenery_score)
+        } else {
+            (0, 0)
+        }
 
-            (&mut state.visible_at_height[0..=height]).fill(1);
-            (&mut state.visible_at_height[(1 + height)..]).iter_mut().for_each(|value| *value += 1);
+    };
 
-            true
-        });
-    }
-
-    (state.visible, state.best_scenic_score)
+    util::parallel::block_on(finish)
 }
 
 
---
+++ src/util/parallel.rs
@@ -0,0 +1,34 @@
+use std::future::Future;
+use std::process::Output;
+use lazy_static::lazy_static;
+use tokio::runtime::Runtime;
+use tokio::task::JoinHandle;
+
+lazy_static! {
+    static ref RUNTIME: Runtime = setup_tokio_runtime();
+}
+
+fn setup_tokio_runtime() -> Runtime {
+    Runtime::new().unwrap()
+}
+
+pub fn ensure_ready() {
+    let handle = RUNTIME.spawn(async {
+        10
+    });
+    RUNTIME.block_on(handle).expect("can complete simple task");
+}
+
+pub fn in_parallel<A, Fut>(body: Fut) -> JoinHandle<A>
+    where A: Send + 'static,
+          Fut: Future<Output=A> + Send + 'static
+{
+    RUNTIME.spawn(body)
+}
+
+pub fn block_on<A, Fut>(body: Fut) -> A
+    where
+        A: Send + 'static,
+        Fut: Future<Output=A> + Send + 'static {
+    RUNTIME.block_on(body)
+}
